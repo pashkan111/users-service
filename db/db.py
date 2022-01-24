eb5d98d32f1f975c19243a00fa3783228c997f3d
pashkan111 <kozhevnikov0001@yandex.ru>
2022-01-25 01:53:55 +0300

    Create app structure

---
+++ db/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5433/test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
+Base = declarative_base()
