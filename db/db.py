da7f23503cb86d2ec4efa0e2b454dab4fcb5b58a
pashkan111 <kozhevnikov0001@yandex.ru>
2022-02-03 22:04:01 +0300

    added alembic

--- db/db.py
+++ db/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@pdb:5432/users"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/users"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
